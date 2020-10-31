020450d124f06b77cae940458fc328f893eeba1f
Anastasiya Ulitina <anastulitina@gmail.com>
2020-10-31 14:07:35 +0300

    Add List

--- HackNews SwiftUI/ContentView.swift
+++ HackNews SwiftUI/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("HackNews")
+        }
     }
 }
 
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Konichiva"),
+    Post(id: "3", title: "Privet")
+    
+]
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
     }
 }
